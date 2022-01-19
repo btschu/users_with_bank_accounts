ac41bdebff7eef1552b335f540ca3475d07bce22
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-01-18 17:51:44 -0700

    cleaned up code

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -1,30 +1,34 @@
 class BankAccount:
     all_accounts = []
-    def __init__(self, int_rate, balance = 0):
+    def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.all_accounts.append(self)
+
     def deposit(self, amount):
         self.balance += amount
         return self
+
     def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient Funds: Charging a $5 Fee")
-            self.balance -= 5
-        else:
+        if(self.balance - amount) >= 0:
             self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee")
+            self.balance -= 5
         return self
+
     def display_account_info(self):
         return self.balance
+
     def yield_interest(self):
         if self.balance > 0:
-            self.balance = self.balance * (1 + self.int_rate)
+            self.balance += (self.balance * self.int_rate)
         return self
 
     @classmethod
     def all_info(cls):
         for account in cls.all_accounts:
-            print("Balance:",account.balance,"Rate:",account.int_rate)
+            print(f"Balance: {account.balance}, Interest Rate: {account.int_rate}")
 
 class User:
     bank_name = "Brandon's Bank"
@@ -35,22 +39,33 @@ class User:
             "Checking Account" : BankAccount(.01,50),
             "Savings Account" : BankAccount(.03,50)
         }
+
     def display_user_balance(self):
         print(f"Member Name: {self.name}, Checking Account Balance: ${self.account['Checking Account'].display_account_info()}")
         print(f"Member Name: {self.name}, Savings Account Balance: ${self.account['Savings Account'].display_account_info()}")
         return self
-    def transfer_money(self, type, other_user, other_user_type, amount):
-        self.accounts[type].withdraw(amount)
-        other_user.accounts[other_user_type].deposit(amount)
+
+    def transfer_money(self,amount,user):
+        self.amount -= amount
+        user.amount += amount
+        self.display_user_balance()
+        user.display_user_balance()
         return self
 
 brandon = User('Brandon Schumacher', 'brandon@gmail.com')
 jacie = User('Jacie Schumacher', 'jacie@gmail.com')
+
 brandon.account["Checking Account"].deposit(200).withdraw(50).withdraw(32).yield_interest()
 brandon.account["Savings Account"].deposit(350).withdraw(50).deposit(5000).yield_interest()
+
 jacie.account["Savings Account"].deposit(500).deposit(350).withdraw(50).yield_interest()
+jacie.account["Checking Account"].withdraw(55).yield_interest()
+
 brandon.display_user_balance()
 jacie.display_user_balance()
+
+
+
 
 #! Instructor's Solution
 
